412d75c82010cdcf02c868b9d5b1b6c8be23bff9
Lucass282 <l.concilio9@studenti.unisa.it>
2025-06-05 12:27:19 +0200

    Adding merchantClaim and some adjustements

--- Phase3/Purchase&Mint.sol
+++ Phase3/Purchase&Mint.sol
@@ -6,21 +6,26 @@ contract PurchaseAndMint {
 
     struct Order {
         string merchantDID;
-        string cost;
+        uint256 cost;
         uint256 expirationDate;
         bytes32 commitment;
     }
 
     mapping(uint256 => Order) private orders; //orderId => order
-    mapping(uint256 => bool) private claimedOrders;
+    mapping(uint256 =>  bool) private claimedOrders;
+    mapping(uint256 => address) private buyers;
+    mapping(uint256 => address) private invokerMerchant; //orderId => merchantAddress
+    mapping(uint256 => bool) private settledOrders;
+    mapping(address => uint256) private escrowedethr; //buyer => ethr escrowed
 
-    event OrderRegistered(uint256 orderId, string shopDID, string price);
-    event OrderClaimed(uint256 orderId, string cost);
+    event OrderRegistered(uint256 orderId, string shopDID, uint256 price);
+    event OrderClaimed(uint256 orderId, address buyer, uint256 cost);
+    event OrderSettled(uint256 orderId);
 
-    function registerOrder(uint256 orderId, string calldata shopDID, string calldata price, uint256 expiration, bytes32 commitment) public { //price dovrebbe essere float
-
+    function registerOrder(uint256 orderId, string calldata shopDID, uint256 price, uint256 expiration, bytes32 commitment) public { 
         require(orders[orderId].expirationDate == 0, "Order already registered!");
-        
+        require(invokerMerchant[orderId] == address(0), "Order already registered!");
+
         orders[orderId] = Order({
             merchantDID : shopDID,
             cost : price,
@@ -28,19 +33,36 @@ contract PurchaseAndMint {
             commitment : commitment
         });
 
+        invokerMerchant[orderId] = msg.sender; 
+
         emit OrderRegistered(orderId, shopDID, price);
     }
 
-    function claimOrder(uint256 orderId, bytes32 orderSecret, string calldata payAmount) public { // payAmount dovrebbe essere un float
+    function claimOrder(uint256 orderId, bytes32 orderSecret, uint256 payAmount) public {
 
         require(orders[orderId].expirationDate != 0, "Order don't exists!");
         require(block.timestamp <= orders[orderId].expirationDate, "Window to claim the order has expired!");
-        require(keccak256(abi.encodePacked(orderId, orderSecret)) == orders[orderId].commitment, "Only true buyer can claims the order!");
+        require(keccak256(abi.encodePacked(orderId, orderSecret)) == orders[orderId].commitment, "Only knower of secret can claims the order!");
         require(claimedOrders[orderId] == false, "Order already claimed!");
-        require(keccak256(abi.encodePacked(orders[orderId].cost)) == keccak256(abi.encodePacked(payAmount)), "Payment amount does not match the cost of the product!");
+        require(orders[orderId].cost == payAmount, "Payment amount does not match the cost of the product!");
 
-        claimedOrders[orderId] == true;
+        claimedOrders[orderId]= true;
+        buyers[orderId] = msg.sender;
+        escrowedethr[msg.sender] == payAmount;
 
-        emit OrderClaimed(orderId, payAmount);
+        emit OrderClaimed(orderId, msg.sender, payAmount);
+    }
+
+    function merchantClaim(uint256 orderId) public {
+        
+        require(invokerMerchant[orderId] == msg.sender, "Only the merchant who originally placed the order can claim it!");
+        require(claimedOrders[orderId] == true, "Order not yet claimed by buyer!");
+        require(settledOrders[orderId] == false, "Order already settled!");
+
+        address buyer = buyers[orderId];
+        escrowedethr[buyer] -= orders[orderId].cost;
+        settledOrders[orderId] = true;
+
+        emit OrderSettled(orderId);
     }
 }
